4e1acf4bed446cc15f5c619e74913e19f1435f95
John P. McCrae <john@mccr.ae>
2016-02-19 14:41:27 +0000

    Update properties

--- src/main/scala/extract.scala
+++ src/main/scala/extract.scala
@@ -17,7 +17,7 @@ object enWordNetExtract {
       groupBy(_._1).mapValues(_.map(x => (x._2, x._3)))
     val synrelations = (root \\ "synsetrelations").filter(
       x => (synsets contains (x \ "@parent").text) && (synsets contains (x \ "@child").text)).map(
-      x => ((x \ "@parent").text, (x \ "@child").text, (x \ "@relation").text)).
+      x => ((x \ "@child").text, (x \ "@parent").text, (x \ "@relation").text)).
       groupBy(_._1).mapValues(_.map(x => (x._2, x._3)))
      (pwn_entries, synsets, lexrelations, synrelations)
   }
@@ -40,166 +40,169 @@ object enWordNetExtract {
     case "przysłówek"  => "r"
   }
 
+    val coreSynRels = Set("agent", "also", "attribute", "be_in_state", "causes", "classified_by", "classifies", "co_agent_instrument", "co_agent_patient", "co_agent_result", "co_instrument_agent", "co_instrument_patient", "co_instrument_result", "co_patient_agent", "co_patient_instrument", "co_result_agent", "co_result_instrument", "co_role", "derivation", "direction", "domain_region", "domain_topic", "domain_usage", "entails", "eq_synonym", "fuzzynym", "has_domain_region", "has_domain_topic", "has_domain_usage", "holo_location", "holo_member", "holo_part", "holo_portion", "holo_substance", "holonym", "hypernym", "hyponym", "in_manner", "instance_hypernym", "instance_hyponym", "instrument", "involved", "involved_agent", "involved_direction", "involved_instrument", "involved_location", "involved_patient", "involved_result", "involved_source_direction", "involved_target_direction", "is_caused_by", "is_entailed_by", "location", "manner_of", "mero_location", "mero_member", "mero_part", "mero_portion", "mero_substance", "meronym", "near_synonym", "other", "patient", "restricted_by", "restricts", "result", "role", "source_direction", "state_of", "target_direction")
+    val coreSenseRels = Set("antonym", "near_antonym", "also", "participle", "pertainym", "derivation", "domain_topic", "has_domain_topic", "domain_region", "has_domain_region", "domain_usage", "has_domain_usage", "other")
+
   def mapRelType(relType : String) = relType match {
     // Relations only in plWordNet
-    case "10" => relType
-    case "11" => relType
-    case "13" => relType
-    case "19" => relType
-    case "20" => relType
-    case "21" => relType
-    case "22" => relType
-    case "23" => relType
-    case "24" => relType
-    case "25" => relType
-    case "26" => relType
-    case "27" => relType
-    case "28" => relType
-    case "29" => relType
-    case "34" => relType
-    case "35" => relType
-    case "36" => relType
-    case "37" => relType
-    case "38" => relType
-    case "39" => relType
-    case "40" => relType
-    case "41" => relType
-    case "42" => relType
-    case "43" => relType
-    case "44" => relType
-    case "45" => relType
-    case "46" => relType
-    case "47" => relType
-    case "48" => relType
-    case "49" => relType
-    case "50" => relType
-    case "51" => relType
-    case "52" => relType
-    case "53" => relType
-    case "55" => relType
-    case "56" => relType
-    case "57" => relType
-    case "58" => relType
-    case "59" => relType
-    case "60" => relType
-    case "62" => relType
-    case "63" => relType
-    case "64" => relType
-    case "65" => relType
-    case "74" => relType
-    case "75" => relType
-    case "89" => relType
-    case "90" => relType
-    case "92" => relType
-    case "93" => relType
-    case "101" => relType
-    case "104" => relType
-    case "106" => relType
-    case "107" => relType
-    case "108" => relType
-    case "110" => relType
-    case "111" => relType
-    case "113" => relType
-    case "114" => relType
-    case "116" => relType
-    case "117" => relType
-    case "118" => relType
-    case "119" => relType
-    case "120" => relType
-    case "121" => relType
-    case "122" => relType
-    case "124" => relType
-    case "125" => relType
-    case "126" => relType
-    case "127" => relType
-    case "129" => relType
-    case "130" => relType
-    case "131" => relType
-    case "134" => relType
-    case "136" => relType
-    case "137" => relType
-    case "138" => relType
-    case "140" => relType
-    case "141" => relType
-    case "142" => relType
-    case "145" => relType
-    case "146" => relType
-    case "147" => relType
-    case "148" => relType
-    case "149" => relType
-    case "151" => relType
-    case "152" => relType
-    case "154" => relType
-    case "155" => relType
-    case "156" => relType
-    case "157" => relType
-    case "158" => relType
-    case "160" => relType
-    case "161" => relType
-    case "163" => relType
-    case "164" => relType
-    case "165" => relType
-    case "166" => relType
-    case "168" => relType
-    case "169" => relType
-    case "194" => relType
-    case "201" => relType
-    case "202" => relType
-    case "203" => relType
-    case "205" => relType
-    case "206" => relType
-    case "207" => relType
-    case "211" => relType
-    case "212" => relType
-    case "214" => relType
-    case "215" => relType
-    case "216" => relType
-    case "217" => relType
-    case "218" => relType
-    case "219" => relType
-    case "225" => relType
-    case "226" => relType
-    case "228" => relType
-    case "229" => relType
-    case "230" => relType
-    case "235" => relType
-    case "238" => relType
-    case "239" => relType
-    case "242" => relType
-    case "244" => relType
-    case "3000" => relType
+    case "10" => "hyponym"
+    case "11" => "hypernym"
+    case "13" => "conversion"
+    case "19" => "fuzzynym"
+    case "20" => "mero_part"
+    case "21" => "mero_portion"
+    case "22" => "mero_location"
+    case "23" => "mero_member"
+    case "24" => "mero_substance"
+    case "25" => "holo_part"
+    case "26" => "holo_portion"
+    case "27" => "holo_location"
+    case "28" => "holo_member"
+    case "29" => "holo_substance"
+    case "34" => "agent"
+    case "35" => "patient"
+    case "36" => "instrument"
+    case "37" => "location"
+    case "38" => "result"
+    case "39" => "time"
+    case "40" => "role"
+    case "41" => "involved_agent"
+    case "42" => "involved_patient"
+    case "43" => "involved_time"
+    case "44" => "involved_location"
+    case "45" => "involved_instrument"
+    case "46" => "involved_result"
+    case "47" => "involved"
+    case "48" => "agent_hidden"
+    case "49" => "location_hidden"
+    case "50" => "result_hidden"
+    case "51" => "be_in_state"
+    case "52" => "state"
+    case "53" => "feminine"
+    case "55" => "young"
+    case "56" => "diminutive"
+    case "57" => "augmentative"
+    case "58" => "inhabitant"
+    case "59" => "derivation"
+    case "60" => "inter_register_synonym"
+    case "62" => "noun_verb_cross_category_synonym"
+    case "63" => "noun_adjective_cross_category_synonym"
+    case "64" => "taxonomic_meronym"
+    case "65" => "taxonomic_holonym"
+    case "74" => "pure_perfective_imperfective"
+    case "75" => "pure_imperfective_perfective"
+    case "89" => "imperfective_verb_adjective_processuality"
+    case "90" => "imperfective_verb_noun_processuality"
+    case "92" => "verb_adjective_state"
+    case "93" => "verb_noun_state"
+    case "101" => "complementary_antonym"
+    case "104" => "proper_antonym"
+    case "106" => "type"
+    case "107" => "instance_hyponym"
+    case "108" => "synset_fuzzynym"
+    case "110" => "secondary_aspect_perfective_imperfective"
+    case "111" => "secondary_aspect_imperfective_perfective"
+    case "113" => "meronym_of_substitution"
+    case "114" => "meronym_of_accompanying_situation"
+    case "116" => "holonym_of_substitution"
+    case "117" => "holonym_of_accompanying_situation"
+    case "118" => "verb_perfective_adjective_processuality"
+    case "119" => "verb_perfective_noun_processuality"
+    case "120" => "cause_imperfective_imperfective"
+    case "121" => "cause_perfective_perfective"
+    case "122" => "perfective_imperfective_cause_of_state"
+    case "124" => "perfective_adjective_cause_of_process"
+    case "125" => "perfective_noun_cause_of_process"
+    case "126" => "imperfective_adjective_cause_of_process"
+    case "127" => "imperfective_noun_cause_of_process"
+    case "129" => "perfective_imperfective"
+    case "130" => "imperfective_imperfective"
+    case "131" => "reflexive_meaning"
+    case "134" => "iterative_imperfective_imperfective"
+    case "136" => "distributive"
+    case "137" => "presuppositional"
+    case "138" => "preceding"
+    case "140" => "iterative_imperfective_perfective"
+    case "141" => "verb_noun_cross_category_synonym"
+    case "142" => "adjective_noun_cross_category_synonym"
+    case "145" => "value_of_attribute"
+    case "146" => "modifier"
+    case "147" => "gradation"
+    case "148" => "similititudinal_meaning"
+    case "149" => "characterizing"
+    case "151" => "comparative"
+    case "152" => "superlative"
+    case "154" => "agent"
+    case "155" => "patient"
+    case "156" => "instrument"
+    case "157" => "location"
+    case "158" => "time"
+    case "160" => "result"
+    case "161" => "cause"
+    case "163" => "potential"
+    case "164" => "habitual"
+    case "165" => "quantitative"
+    case "166" => "evaluation"
+    case "168" => "markedness_of_intensity"
+    case "169" => "cross_category_synonym_for_relational_adjectives"
+    case "194" => "participle_of_verb"
+    case "201" => "holo_part"
+    case "202" => "holo_member"
+    case "203" => "holo_madeof"
+    case "205" => "mero_part"
+    case "206" => "mero_member"
+    case "207" => "mero_substance"
+    case "211" => "hypernym"
+    case "212" => "hyponym"
+    case "214" => "holo_part"
+    case "215" => "holo_member"
+    case "216" => "holo_substance"
+    case "217" => "mero_part"
+    case "218" => "mero_member"
+    case "219" => "mero_madeof"
+    case "225" => "near_synonym"
+    case "226" => "near_synonym"
+    case "228" => "inter_register_synonym"
+    case "229" => "inter_register_synonym"
+    case "230" => "instance_hypernym" // To Sumo
+    case "235" => ""
+    case "238" => ""
+    case "239" => ""
+    case "242" => ""
+    case "244" => ""
+    case "3000" => "" // ???
     // Relations in both resources
     case "170" => "antonym"
     case "171" => "hyponym"
     case "172" => "instance_hyponym"
     case "173" => "hypernym"
     case "174" => "instance_hypernym"
-    case "175" => "member_meronym"
-    case "176" => "substance_meronym"
-    case "177" => "part_meronym"
-    case "178" => "member_holonym"
-    case "179" => "substance_holonym"
-    case "180" => "part_holonym"
+    case "175" => "mero_member"
+    case "176" => "mero_substance"
+    case "177" => "mero_part"
+    case "178" => "holo_member"
+    case "179" => "holo_substance"
+    case "180" => "holo_part"
     case "181" => "attribute"
     case "182" => "derivation"
-    case "183" => "domain_member_category"
-    case "184" => "domain_category"
-    case "185" => "domain_member_region"
+    case "183" => "has_domain_topic"
+    case "184" => "domain_topic"
+    case "185" => "has_domain_region"
     case "186" => "domain_region"
-    case "187" => "domain_member_usage"
+    case "187" => "has_domain_usage"
     case "188" => "domain_usage"
-    case "189" => "189"
-    case "190" => "190"
-    case "191" => "191"
-    case "192" => "192"
-    case "193" => "similar"
-    case "195" => "195"
-    case "208" => "208"
-    case "209" => "209"
-    case "210" => "210"
-    case "213" => "213"
-    case "222" => "222"
-    case "223" => "223"
+    case "189" => "entails"
+    case "190" => "causes"
+    case "191" => "also"
+    case "192" => "fuzzynym"
+    case "193" => "fuzzynym"
+    case "195" => "pertainym"
+    case "208" => "eq_synonym" // eq_synonym2
+    case "209" => "eq_synonym" // eq_synonym3
+    case "210" => "hypernym" // hypernym
+    case "213" => "hyponym" // hyponym
+    case "222" => "" // automatic_prompt2
+    case "223" => "" // automatic_prompt3
   }
 
   def load_pwn : (Map[String, Seq[String]], Map[String, Seq[String]], Map[String, String]) = {
@@ -304,8 +307,14 @@ object enWordNetExtract {
                synset="${entry_mapping.getOrElse(entryid, "pl-" + sense)}">""")
           for((targ, relType) <- lexrelations.getOrElse(entryid, Nil)) {
             val tid = entry_mapping.getOrElse(targ, "pl-" + targ)
-            out.print(s"""
-          <SenseRelation target="${tid}" relType="${mapRelType(relType)}"/>""")
+            val t = mapRelType(relType)
+            if(coreSenseRels contains t) {
+              out.print(s"""
+          <SenseRelation target="${tid}" relType="$t"/>""")
+            } else if(t != "") {
+              out.print(s"""
+          <SenseRelation target="${tid}" relType="other" dc:type="$t"/>""")
+            }
           }
           out.print(s"""
         </Sense>""")
@@ -322,8 +331,14 @@ object enWordNetExtract {
         <Definition>${defn}</Definition>""")
       for((targ, rel) <- synrelations.getOrElse(synsetid, Nil)) {
         val tid = synset_mapping.getOrElse(targ, "pl-" + targ)
-        out.print(s"""
-        <SynsetRelation target="${tid}" relType="${mapRelType(rel)}"/>""")
+        val t = mapRelType(rel)
+        if(coreSynRels contains t) {
+          out.print(s"""
+        <SynsetRelation target="${tid}" relType="$t"/>""")
+        } else if(t != "") {
+          out.print(s"""
+        <SynsetRelation target="${tid}" relType="other" dc:type="$t"/>""")
+        }
       }
       out.println("""
       </Synset>""")
@@ -331,6 +346,8 @@ object enWordNetExtract {
     out.println("""
     </Lexicon>
   </LexicalResource>""")
+    out.flush
+    out.close
   }
 
   def main(args : Array[String]) {
